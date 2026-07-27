98089d5f6bb020f0e607aeebd12a294fed5a24d8
Krishna Goenka <krishnagoenka976@gmail.com>
2026-07-27 18:57:04 +0530

    Week 1 Day 1

---
+++ Day 1/Day 1.py
@@ -0,0 +1,43 @@
+# Program 1
+print("Hello World")
+
+#Program 2
+print("Krishna Goenka")
+print("Chandigarh University")
+print("BE.CSE.CSBS")
+
+# Program 3
+name = "Krishna"
+print("Hello" , name+"!")
+print("Welcome to Python.")
+
+# Program 4
+a = 12
+b = 12
+print( a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+# Program 5
+import math
+r = 2
+print("Area of the circle is : ", math.pi *r*r)
+
+# Program 6
+a = 10
+b = 20 
+a,b = b,a
+print(a)
+print(b)
+
+# Program 7
+print("=================================")
+print("        Student Profile")
+print("=================================")
+
+print("Name    : Krishna Goenka")
+print("Age     : 20")
+print("College : Chandigarh University")
+print("Course  : B.Tech CSE (CSBS)")
+print("Skills  : Python")
