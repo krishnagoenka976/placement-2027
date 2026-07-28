d3dc3e08e3a2307f88836b688de255d6f2eadb10
Krishna Goenka <krishnagoenka976@gmail.com>
2026-07-28 19:12:07 +0530

    Week 1 Day 2

---
+++ Day 2/Day 2.py
@@ -0,0 +1,46 @@
+# name = input("Enter your name : ")
+# print("Hello " + name + "!")
+# print("Welcome to Python.")
+
+# name = input("Enter you name:")
+# age = input("Enter you age:")
+# college = input("Enter you college:")
+# course = input("Enter you course:")
+
+# print("===========================")
+# print("Name    : ",name)
+# print("Age     : ",age)
+# print("College : ",college)
+# print("Course  : ",course)
+
+
+# a = int(input("Enter a number : "))
+# b = int(input("Enter a number : "))
+# print("The number you entered is : ", a)
+# print("The number you entered is : ", b)
+
+# print("Sum of",a,"and",b,"is : ", a+b)
+# print("Difference of",a,"and",b,"is : ", a-b)
+# print("Product of",a,"and",b,"is : ", a*b)
+# print("Division of",a,"and",b,"is : ", a/b)
+
+# import math
+# r = int(input("Enter the radius:"))
+# print("Area of the circle is , " ,math.pi*r*r)
+
+
+# name = input("Enter a name:")
+# print("Length = " , len(name))
+
+# name = input("Enter a name:")
+# print("First character = ", name[0])
+# print("Last character = ", name[len(name)-1])
+
+# name = input("Enter a name:")
+# print(name[0:5])
+# print(name[2:8])
+# print(name[0:3])
+# print(name[-3:])
+
+name = input("Enter a name:")
+print(name[::-1])
