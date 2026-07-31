893d7edc6c9dcdd2777939395869534cf01d638c
Krishna Goenka <krishnagoenka976@gmail.com>
2026-07-31 19:21:35 +0530

    Week 1 Day 5

---
+++ Week 1/Day 5/Day 5.py
@@ -0,0 +1,59 @@
+# i = 1
+# while (i<=10):
+#     print(i)
+#     i+=1
+
+# # Prrogram 2
+# i = 10
+# while(i>0):
+#     print(i)
+#     i-=1
+
+# num = int(input("Enter a number: "))
+# i = 1
+# while(i<=10):
+#     print(num,"X",i,"=",num*i)
+#     i+=1
+
+# i = 1
+# while(i<21):
+#     if (i%2==0):
+#         print(i)
+#     i+=1
+
+# i = 1
+# while(i<21):
+#     if (i%2!=0):
+#         print(i)
+#     i+=1
+
+# passw = input("Enter your password: ")
+# while(passw!="python123"):
+#     passw = input("Enter your password: ")
+# print("Password accepted")
+
+
+# i = 0
+# while(i<10):
+#     i+=1
+#     if(i==5):
+#         continue
+#     print(i)       
+    
+
+# i = 0
+# while(i<10):
+#     i+=1
+#     if(i==8):
+#         break
+#     print(i)
+
+i = 1
+
+while(i<=5):
+    j = 1
+    while(j<=i):
+        print("*",end="")
+        j+=1
+    print()
+    i+=1
